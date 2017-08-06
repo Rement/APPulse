7241298fc05cc7c090bd3c562d77acd815fa3fe6
Nadezhda <nadezhda.kosareva95@gmail.com>
2017-08-06 23:36:47 +0300

    First commit

---
+++ src/main/java/by/iba/APPulse/ApPulseApplication.java
@@ -0,0 +1,12 @@
+package by.iba.APPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApPulseApplication.class, args);
+	}
+}
